defc2c31985eb4d4517eb9b9c115367296ac348f
Jevoughn Wisdom <jevoughnwisdom@gmail.com>
2020-07-15 03:54:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,11 +3,6 @@ def unsafe?(speed)
   speed < 40 || speed > 60 ? true : false
 end
 end
-
-
-
-#use a ternary operator to define whether the conditions are unsafe
-def not_safe?(speed)
 def not_safe?(speed)
   speed < 40 || speed > 60 ? "unsafe" : "safe"
   speed < 40 || speed > 60 ? true : false
