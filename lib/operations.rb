d778984d0368a5775ba34b47910f812ebb2de025
Jevoughn Wisdom <jevoughnwisdom@gmail.com>
2020-07-15 04:37:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
- if speed < 40
-  true
-if speed > 90
- true
-else
-  false
+  if speed > 60
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 end
 
 def not_safe?(speed)
-  speed < 40 || speed > 90 ? true : false
-
+  speed < 40 || speed > 60 ? true : false
 end
